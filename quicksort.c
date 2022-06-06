fa279e06a9031ed5c9e4e08441f04b5be007fa05
angelo iossa <iossa@192.168.68.115>
2022-06-06 22:09:45 +0200

    add al new files

---
+++ quicksort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "quicksort.h"
+
+/*
+ *  Swap function 
+*/
+void swap (int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+/*
+ *  Print array function 
+*/
+void print_array (int *array)
+{
+    int i;
+    printf("Array: ");
+    for (i = 0; i < LENGHT; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+}
+
+
+/*
+ *  Partition function 
+*/
+int partition (int *array, int first, int last)
+{
+    
+}
---
+++ quicksort.h
@@ -0,0 +1,8 @@
+#ifndef __QUICKSORT_H__
+#define __QUICKSORT_H__
+
+void swap (int *a, int *b);
+void print_array (int *array);
+
+
+#endif
